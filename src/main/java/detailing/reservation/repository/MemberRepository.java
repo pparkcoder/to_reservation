27211b3d7560029c874d0df01354d02a578a3313
parkjanghoon <pparkcoder@gmail.com>
2024-09-24 20:26:00 +0900

    회원 서비스 구현

--- src/main/java/detailing/reservation/repository/MemberRepository.java
+++ src/main/java/detailing/reservation/repository/MemberRepository.java
@@ -3,15 +3,16 @@ package detailing.reservation.repository;
 import detailing.reservation.domain.Member;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member){
         em.persist(member);
