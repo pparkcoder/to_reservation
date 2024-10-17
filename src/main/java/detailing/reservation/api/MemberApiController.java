427d5efa295f733c1e80e631c80caf60a5f69a4b
parkjanghoon <pparkcoder@gmail.com>
2024-10-17 19:41:57 +0900

    회원 수정 API 구현

--- src/main/java/detailing/reservation/api/MemberApiController.java
+++ src/main/java/detailing/reservation/api/MemberApiController.java
@@ -4,12 +4,11 @@ import detailing.reservation.domain.Member;
 import detailing.reservation.service.MemberService;
 import jakarta.validation.Valid;
 import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,6 +30,27 @@ public class MemberApiController {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request){
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
+    }
+
 
     @Data
     static class CreateMemberRequest{
