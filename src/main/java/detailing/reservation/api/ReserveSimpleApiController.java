e5b8cb794e7c5f84138ff8734ec054e15e1b4e3f
parkjanghoon <pparkcoder@gmail.com>
2024-10-24 20:28:17 +0900

    예약조회 API구현(DTO 사용)

--- src/main/java/detailing/reservation/api/ReserveSimpleApiController.java
+++ src/main/java/detailing/reservation/api/ReserveSimpleApiController.java
@@ -1,13 +1,18 @@
 package detailing.reservation.api;
 
+import detailing.reservation.domain.Address;
 import detailing.reservation.domain.Reserve;
 import detailing.reservation.domain.ReserveSearch;
+import detailing.reservation.domain.ReserveStatus;
 import detailing.reservation.repository.ReserveRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne 최적화
@@ -26,5 +31,31 @@ public class ReserveSimpleApiController {
             reserve.getMember().getName(); // Lazy 강제 초기화
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-reserves")
+    public List<SimpleReserveDto> reserveV2(){
+        List<Reserve> reserves = reserveRepository.findAllByString(new ReserveSearch());
+        List<SimpleReserveDto> result = reserves.stream()
+                .map(r -> new SimpleReserveDto(r))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleReserveDto{
+        private Long reserveId;
+        private String name;
+        private LocalDateTime reserveData;
+        private ReserveStatus reserveStatus;
+        private Address address;
+
+        public SimpleReserveDto(Reserve reserve) {
+            reserveId = reserve.getId();
+            name = reserve.getMember().getName();
+            reserveData = reserve.getReserveDate();
+            reserveStatus = reserve.getStatus();
+            address = reserve.getMember().getAddress();
+        }
     }
 }
