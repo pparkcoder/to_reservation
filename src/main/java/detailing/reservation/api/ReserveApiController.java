9fdc866d037ddd19799dd7d9f167590c7ade06dd
parkjanghoon <pparkcoder@gmail.com>
2024-10-29 20:32:14 +0900

    예약 리스트 조회 API 구현(DTO 사용)

--- src/main/java/detailing/reservation/api/ReserveApiController.java
+++ src/main/java/detailing/reservation/api/ReserveApiController.java
@@ -1,14 +1,16 @@
 package detailing.reservation.api;
 
-import detailing.reservation.domain.Reserve;
-import detailing.reservation.domain.ReserveSearch;
-import detailing.reservation.domain.ReserveShop;
+import detailing.reservation.domain.*;
 import detailing.reservation.repository.ReserveRepository;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +27,48 @@ public class ReserveApiController {
             reserveShops.stream().forEach(r -> r.getShop().getName());
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/reserves")
+    public List<ReserveDto> reserveV2(){
+        List<Reserve> reserves = reserveRepository.findAllByString(new ReserveSearch());
+        List<ReserveDto> collect = reserves.stream()
+                .map(r -> new ReserveDto(r))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @Data
+    static class ReserveDto{
+
+        private Long reserveId;
+        private String name;
+        private LocalDateTime reserveDate;
+        private ReserveStatus reserveStatus;
+        private Address address;
+        private List<ReserveShopDto> reserveShops;
+
+        public ReserveDto(Reserve reserve) {
+            reserveId = reserve.getId();
+            name = reserve.getMember().getName();
+            reserveDate = reserve.getReserveDate();
+            reserveStatus = reserve.getStatus();
+            address = reserve.getMember().getAddress();
+            reserveShops = reserve.getReserveShops().stream()
+                    .map(reserveShop -> new ReserveShopDto(reserveShop))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    static class ReserveShopDto {
+        private String shopName;
+        private int price;
+        private int count;
+        public ReserveShopDto(ReserveShop reserveShop) {
+            shopName = reserveShop.getShop().getName();
+            price = reserveShop.getReservePrice();
+            count = reserveShop.getReserveCount();
+        }
     }
 }
